790c753866a4017ffc6a62c6e8e5d1b364f13588
vandenbrande <contact@arduinosensors.nl>
2017-07-21 03:21:11 +0200

    Create lm35_TEMPsensor_C#_Application.ino

---
+++ lm35_TEMPsensor_C#_Application.ino
@@ -0,0 +1,23 @@
+const int analogIn = A0;
+
+int RawValue= 0;
+double Voltage = 0;
+double tempC = 0;
+double tempF = 0;
+
+void setup(){  
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  RawValue = analogRead(analogIn);
+  Voltage = (RawValue / 1023.0) * 5000; // 5000 to get millivots.
+  tempC = Voltage * 0.1; // 500 is the offset
+  tempF = (tempC * 1.8) + 32; // conver to F      
+  
+
+  Serial.println(tempC,1);
+//  Serial.println(tempF,1);//uncomment for fahrenheit
+  delay(500);  
+}
