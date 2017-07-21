e89a13f60e5a2f1952a51abccf0a46e6f5c5f49f
vandenbrande <contact@arduinosensors.nl>
2017-07-21 03:25:04 +0200

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BlueCore Tech LM35 Temp Meter")]
+[assembly: AssemblyDescription("BlueCore Tech LM35 Temp Meter is a open source app created for arduino. This app basicly can be used for any sensor, as long as you send the value to the c# app from the arduino over the serial port. If you cannot find the source code send a mail to contact@arduinosensors.nl")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BlueCore Tech")]
+[assembly: AssemblyProduct("BlueCore Tech LM35 Temp Meter")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("40a119d9-d378-4f44-96c1-675770a7fb42")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
