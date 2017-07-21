a85068d3bf22814e992b6f70bc30ce1ac3f89035
vandenbrande <contact@arduinosensors.nl>
2017-07-21 03:20:11 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace SONAR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtCOM = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.codeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.fBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.bLOGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.groupBox1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+			this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(17, 21);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(40, 42);
+			this.button1.TabIndex = 0;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(14, 30);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 15);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Type in COM port";
+			// 
+			// txtCOM
+			// 
+			this.txtCOM.BackColor = System.Drawing.SystemColors.GrayText;
+			this.txtCOM.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.txtCOM.Location = new System.Drawing.Point(130, 27);
+			this.txtCOM.Name = "txtCOM";
+			this.txtCOM.Size = new System.Drawing.Size(62, 22);
+			this.txtCOM.TabIndex = 5;
+			this.txtCOM.Text = "COM12";
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Transparent;
+			this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+			this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+			this.button2.Location = new System.Drawing.Point(59, 21);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(41, 42);
+			this.button2.TabIndex = 6;
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.txtCOM);
+			this.groupBox1.Location = new System.Drawing.Point(6, 117);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(376, 55);
+			this.groupBox1.TabIndex = 7;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Connect to Arduino Serial (COM)";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Transparent;
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.Controls.Add(this.groupBox3);
+			this.panel1.Controls.Add(this.groupBox1);
+			this.panel1.Controls.Add(this.groupBox2);
+			this.panel1.Location = new System.Drawing.Point(12, 37);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(395, 181);
+			this.panel1.TabIndex = 8;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.button1);
+			this.groupBox2.Controls.Add(this.button2);
+			this.groupBox2.Location = new System.Drawing.Point(6, 4);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(114, 77);
+			this.groupBox2.TabIndex = 10;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Control Panel";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.SystemColors.Desktop;
+			this.label1.Font = new System.Drawing.Font("DigiGrotesk LT BoldCondensed", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(171, 85);
+			this.label1.TabIndex = 13;
+			this.label1.Text = "00.0";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.menuStrip1.Font = new System.Drawing.Font("DigiGrotesk LT BoldCondensed", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.codeToolStripMenuItem,
+            this.fBToolStripMenuItem,
+            this.bLOGToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+			this.menuStrip1.Size = new System.Drawing.Size(417, 24);
+			this.menuStrip1.TabIndex = 9;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+			this.aboutToolStripMenuItem.Text = "About";
+			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+			// 
+			// codeToolStripMenuItem
+			// 
+			this.codeToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("codeToolStripMenuItem.Image")));
+			this.codeToolStripMenuItem.Name = "codeToolStripMenuItem";
+			this.codeToolStripMenuItem.Size = new System.Drawing.Size(28, 20);
+			this.codeToolStripMenuItem.Click += new System.EventHandler(this.codeToolStripMenuItem_Click);
+			// 
+			// fBToolStripMenuItem
+			// 
+			this.fBToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.fBToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("fBToolStripMenuItem.Image")));
+			this.fBToolStripMenuItem.Name = "fBToolStripMenuItem";
+			this.fBToolStripMenuItem.Size = new System.Drawing.Size(28, 20);
+			this.fBToolStripMenuItem.Text = "FB";
+			this.fBToolStripMenuItem.Click += new System.EventHandler(this.fBToolStripMenuItem_Click);
+			// 
+			// bLOGToolStripMenuItem
+			// 
+			this.bLOGToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.bLOGToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("bLOGToolStripMenuItem.Image")));
+			this.bLOGToolStripMenuItem.Name = "bLOGToolStripMenuItem";
+			this.bLOGToolStripMenuItem.Size = new System.Drawing.Size(28, 20);
+			this.bLOGToolStripMenuItem.Text = "Youtube";
+			this.bLOGToolStripMenuItem.Click += new System.EventHandler(this.bLOGToolStripMenuItem_Click);
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.panel3);
+			this.groupBox3.Controls.Add(this.pictureBox1);
+			this.groupBox3.Location = new System.Drawing.Point(126, 3);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(256, 112);
+			this.groupBox3.TabIndex = 10;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Temperature Monitor";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pictureBox1.Location = new System.Drawing.Point(6, 17);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(67, 90);
+			this.pictureBox1.TabIndex = 14;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("DigiGrotesk LT BoldCondensed", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+			this.label3.Location = new System.Drawing.Point(7, 6);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 15);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "NOT CONNECTED";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.Desktop;
+			this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel2.Controls.Add(this.label3);
+			this.panel2.Location = new System.Drawing.Point(6, 87);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(114, 28);
+			this.panel2.TabIndex = 11;
+			// 
+			// panel3
+			// 
+			this.panel3.BackColor = System.Drawing.Color.Black;
+			this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel3.Controls.Add(this.label1);
+			this.panel3.Location = new System.Drawing.Point(79, 17);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(171, 90);
+			this.panel3.TabIndex = 15;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(417, 225);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.menuStrip1);
+			this.Font = new System.Drawing.Font("DigiGrotesk LT BoldCondensed", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuStrip1;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "BlueCore Tech LM35 Temperature Meter";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCOM;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem codeToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolStripMenuItem fBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bLOGToolStripMenuItem;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace SONAR
+{
+    public partial class Form1 : Form
+    {
+        SerialPort _serialPort;
+        delegate void SetTextDelegate(string value); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             _serialPort = new SerialPort();
+             _serialPort.BaudRate = 9600;
+             _serialPort.Parity = Parity.None;
+             _serialPort.DataBits = 8;
+             _serialPort.ReadTimeout = 500;
+             _serialPort.WriteTimeout = 500;
+             _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _serialPort.PortName = txtCOM.Text;
+                if (!_serialPort.IsOpen)
+                    _serialPort.Open();
+				label3.Text = "CONNECTION OK";
+				label3.ForeColor = System.Drawing.Color.Lime;
+
+			}
+            catch
+            {
+                MessageBox.Show("Fill in the right Serial Port (COM) Or Disconnect first");
+            }
+
+
+        }
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (_serialPort.IsOpen)
+				_serialPort.Close();
+			label3.Text = "NOT CONNECTED";
+			label3.ForeColor = System.Drawing.Color.Red;
+			label1.Text = "00.0";
+
+		}
+
+		public void SetText(string value)
+        {
+            if (InvokeRequired)
+                try
+                {
+                    Invoke(new SetTextDelegate(SetText), value);
+                }
+                catch { }
+            else
+			label1.Text = value;
+                
+        } 
+
+        private  void DataReceivedHandler(object sender,SerialDataReceivedEventArgs e)
+        {
+            string indata = _serialPort.ReadLine();
+            SetText(indata);
+        }
+
+
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 box = new AboutBox1();
+            box.ShowDialog();
+        }
+
+        private void codeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.instructables.com/member/vandenbrande/instructables/");
+        }
+
+        private void fBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/ArduinoProjects101/");
+        }
+
+        private void bLOGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/channel/UCzk_SWD_SASF8UE2FPk9LOA");
+        }
+
+       
+    }
+}
